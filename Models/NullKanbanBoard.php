88fa7d67d17a945424ee369c1a6b30e7d9fe9c7c
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-09-19 21:24:04 +0200

    add null module which fixes a bug where no board was available

---
+++ Models/NullKanbanBoard.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.4
+ *
+ * @package   Modules\Kanban\Models
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace Modules\Kanban\Models;
+
+/**
+ * Null model
+ *
+ * @package Modules\Kanban\Models
+ * @license OMS License 1.0
+ * @link    https://orange-management.org
+ * @since   1.0.0
+ */
+final class NullKanbanBoard extends KanbanBoard
+{
+    /**
+     * Constructor
+     *
+     * @param int $id Model id
+     *
+     * @since 1.0.0
+     */
+    public function __construct(int $id = 0)
+    {
+        $this->id = $id;
+    }
+}
