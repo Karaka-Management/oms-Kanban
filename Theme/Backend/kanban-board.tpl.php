135e841b064aa4d56d9aefc5c82f5ea1368ac533
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-06-18 14:56:00 +0200

    Update kanban-board.tpl.php

--- Theme/Backend/kanban-board.tpl.php
+++ Theme/Backend/kanban-board.tpl.php
@@ -1,5 +1,15 @@
 <?php
-
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @package   Modules\Kanban
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
 declare(strict_types=1);
 
 /** @var \Modules\Kanban\Models\KanbanBoard $board */
