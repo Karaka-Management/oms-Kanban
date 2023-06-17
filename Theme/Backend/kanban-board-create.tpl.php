61a4acd437025ce6cb2ec93b34a49f3bfe5ebd7a
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-06-17 14:16:43 +0000

    start creating create form

--- Theme/Backend/kanban-board-create.tpl.php
+++ Theme/Backend/kanban-board-create.tpl.php
@@ -12,3 +12,31 @@
  */
 declare(strict_types=1);
 echo $this->data['nav']->render();
+?>
+<div class="row">
+    <div class="col-xs-12 col-md-6 col-lg-4">
+        <section class="portlet">
+            <form action="<?= \phpOMS\Uri\UriFactory::build('{/api}...'); ?>" method="post">
+                <div class="portlet-head"><?= $this->getHtml('Kanban'); ?></div>
+                <div class="portlet-body">
+                    <div class="form-group">
+                        <label for="iTitle"><?= $this->getHtml('Name'); ?></label>
+                        <input id="iTitle" type="text" name="title" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="iDescription"><?= $this->getHtml('Description'); ?></label>
+                        <textarea id="iDescription" name="description"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="iTags"><?= $this->getHtml('Tags'); ?></label>
+                    </div>
+                </div>
+                <div class="portlet-foot">
+                    <input type="Submit" value="<?= $this->getHtml('Create', '0', '0'); ?>">
+                </div>
+            </form>
+        </section>
+    </div>
+</div>
