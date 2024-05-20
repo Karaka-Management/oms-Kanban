9136fbabd3b1aba19463988608a099f8203d3b6f
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-05-21 00:09:12 +0200

    fix version and bugs

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\Kanban
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -40,7 +40,7 @@ use phpOMS\Utils\Parser\Markdown\Markdown;
  * Kanban controller class.
  *
  * @package Modules\Kanban
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
--- Models/KanbanBoard.php
+++ Models/KanbanBoard.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\Kanban\Models
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -22,7 +22,7 @@ use Modules\Tag\Models\Tag;
  * Kanban board class.
  *
  * @package Modules\Kanban\Models
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
